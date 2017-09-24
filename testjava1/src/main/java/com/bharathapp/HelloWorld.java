7d4630d2e8b4476bf6605b56caa1eb4881dd446c
root <root@bharath.bash.com>
2017-09-24 05:23:58 +0000

    working with Typed tasks like Copy copySpec and expand

---
+++ testjava1/src/main/java/com/bharathapp/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.bharathapp;
+
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+            System.out.println("Hello, World, Hey this is bharath good mng");
+               }
+               }
