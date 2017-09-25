18959e6f353f748835f918b119776efe3c488327
root <root@bharath.bash.com>
2017-09-25 03:21:34 +0000

    Gradle with dependencies repositories and jar versions

--- bharathjava/testjava1/src/main/java/com/bharathapp/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.bharathapp;
-
-public class HelloWorld {
-   public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-            System.out.println("Hello, World, Hey this is bharath good mng");
-               }
-               }
---
+++ javaproject/src/main/java/com/bharathwebapp/repository/Greeter.java
@@ -0,0 +1,7 @@
+package com.bharathwebapp.repository;
+
+public class Greeter {
+  public String sayHello() {
+    return "Hello world!";
+  }
+}
